c8b52a437fd21259b8c92cec9b5f35664f170d75
wxm <42549882@qq.com>
2015-09-17 22:05:16 +0800

    wxm

--- source/app/lang/zh_CN/button.php
+++ source/app/lang/zh_CN/button.php
@@ -6,5 +6,9 @@ return array(
 	'delete' => '删除',
 	'details' => '补充详细资料',
 	'basics' => '修改基本资料',
-    'printadmissionform' => '打印报名表'
+    'printadmissionform' => '打印报名表',
+    'submit' => '提交',
+    'add' => '增加',
+    'agree' => '同意',
+    'ok'     => '确定'
 );
--- source/app/lang/zh_CN/general.php
+++ source/app/lang/zh_CN/general.php
@@ -1,12 +1,17 @@
 <?php
 return array (
 		
-		'yes' => '是',
-		'no' => '否',
-		'must_login' => 'Must be logged in.',
-		'back' => '返回',
-		'cancel' => '取消',
-		'delete' => '刪除',
-        'pleaseselect' => '请选择'
+    'yes' => '是',
+    'no' => '否',
+    'must_login' => 'Must be logged in.',
+    'back' => '返回',
+    'cancel' => '取消',
+    'delete' => '刪除',
+    'pleaseselect' => '请选择',
+    'nopass'    => '不通过',
+    'pass'  => '通过',
+    'nocheck' => '未审核',
+    'ok' => '确认',
+    'add' => '添加'
 )
 ;
